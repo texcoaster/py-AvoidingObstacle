2b7180a6c8554801618a6d9e33f165256cb7381e
HajunPark <phajun105@naver.com>
2022-11-13 18:33:59 +0900

    arrow -> obstacle

--- obstacle.py
+++ obstacle.py
@@ -3,8 +3,5 @@ import pygame
 class Arrow(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
-
-    self.image = pygame.image.load("images/arrow.png")
-
     self.rect = self.image.get_rect()
     self.rect.topleft = (0, 0)
